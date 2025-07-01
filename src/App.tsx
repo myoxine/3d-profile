b4284f302097d283ee14ff1275d7bec28a6ac348
Hadi Halim <hdi_hlim@yahoo.com>
2025-07-01 14:38:03 +0700

    Step 5: Add Lighting with Shadows

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { Room } from './components/Room';
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas flat linear camera={{ position: [0, 2, 5], fov: 60 }}>
+      <Canvas flat linear camera={{ position: [0, 2, 5], fov: 60 }} shadows>
         <ambientLight intensity={1} />
-        <directionalLight position={[0, 2, 5]} intensity={1}   />
+        <directionalLight position={[0, 2, 5]} intensity={1} castShadow />
         <OrbitControls />
         <Room />
       </Canvas>
--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -2,27 +2,27 @@ export const Room = () => {
     return (
         <>
             {/* Floor */}
-            <mesh position={[0, 0, 0]}>
+            <mesh position={[0, 0, 0]} receiveShadow>
                 <boxGeometry args={[10, 0.1, 10]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
 
             {/* Back Wall */}
-            <mesh position={[0, 2.5, -5]}>
+            <mesh position={[0, 2.5, -5]} receiveShadow>
                 <boxGeometry args={[10, 5, 0.1]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
 
             {/* Left Wall */}
-            <mesh position={[-5, 2.5, 0]}>
+            <mesh position={[-5, 2.5, 0]} receiveShadow>
                 <boxGeometry args={[0.1, 5, 10]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
 
             {/* A Box (Maybe a chair or table?) */}
-            <mesh position={[0, 0.55, 0]}>
+            <mesh position={[0, 0.55, 0]} castShadow>
                 <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="orange" />
+                <meshStandardMaterial color="red" />
             </mesh>
         </>
     );
