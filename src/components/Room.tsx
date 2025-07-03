104a4e995223ea8941260c732fd42248eb78f36d
Hadi Halim <hdi_hlim@yahoo.com>
2025-07-03 15:40:27 +0700

    Step 2 - Create Realistic Walls & Wooden Floor

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,28 +1,14 @@
+import { Wall } from './Wall'
+import { Floor } from './Floor'
 export const Room = () => {
+
     return (
         <>
             {/* Floor */}
-            <mesh position={[0, 0.05, 0]} receiveShadow>
-                <boxGeometry args={[10, 0.1, 10]} />
-                <meshStandardMaterial color="#ffffff" />
-            </mesh>
-
-            {/* Back Wall */}
-            <mesh position={[0, 2.5, -5.05]} receiveShadow>
-                <boxGeometry args={[10, 5, 0.1]} />
-                <meshStandardMaterial color="#ffffff" />
-            </mesh>
-
-            {/* Left Wall */}
-            <mesh position={[-5.05, 2.5, -0.05]} receiveShadow>
-                <boxGeometry args={[0.1, 5, 10.1]} />
-                <meshStandardMaterial color="#ffffff" />
-            </mesh>
-            {/* right Wall */}
-            <mesh position={[5.05, 2.5, -0.05]} receiveShadow>
-                <boxGeometry args={[0.1, 5, 10.1]} />
-                <meshStandardMaterial color="#ffffff" />
-            </mesh>
+            <Floor />
+            <Wall position={[0, 2.5, -5.05]} />
+            <Wall position={[-5.05, 2.5, -0.05]} size={[0.1, 5, 10.1]} />
+            <Wall position={[5.05, 2.5, -0.05]} size={[0.1, 5, 10.1]} />
             {/* A Box (Maybe a chair or table?) */}
             <mesh position={[0, 0.55, 0]} castShadow>
                 <boxGeometry args={[1, 1, 1]} />
