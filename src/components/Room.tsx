260e9d986801021f716d08d4e5a6b03bbdc62fc6
Hadi Halim <hdi_hlim@yahoo.com>
2025-07-08 12:02:48 +0700

    room position

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -2,29 +2,29 @@ export const Room = () => {
     return (
         <>
             {/* Floor */}
-            <mesh position={[0, 0.05, 0]} receiveShadow>
+            <mesh position={[0, -2 + 0.05, 0]} receiveShadow>
                 <boxGeometry args={[3, 0.1, 3]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
 
             {/* Back Wall */}
-            <mesh position={[0, 2, -1.55]} receiveShadow>
+            <mesh position={[0, 0, -1.55]} receiveShadow>
                 <boxGeometry args={[3, 4, 0.1]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
 
             {/* Left Wall */}
-            <mesh position={[-1.55, 2, -0.05]} receiveShadow>
+            <mesh position={[-1.55, 0, -0.05]} receiveShadow>
                 <boxGeometry args={[0.1, 4, 3.1]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
             {/* right Wall */}
-            <mesh position={[1.55, 2, -0.05]} receiveShadow>
+            <mesh position={[1.55, 0, -0.05]} receiveShadow>
                 <boxGeometry args={[0.1, 4, 3.1]} />
                 <meshStandardMaterial color="#ffffff" />
             </mesh>
             {/* A Box (Maybe a chair or table?) */}
-            <mesh position={[0, 0.55, 0]} castShadow>
+            <mesh position={[0, -2 +0.55, 0]} castShadow>
                 <boxGeometry args={[1, 1, 1]} />
                 <meshStandardMaterial color="red" />
             </mesh>
