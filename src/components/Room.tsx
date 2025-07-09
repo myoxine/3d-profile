aeac6e679ba08b8b37187a5588b706a67f5b38b1
Hadi Halim <hdi_hlim@yahoo.com>
2025-07-09 14:37:04 +0700

    final part 2

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,18 +1,23 @@
 import { Floor } from './Floor'
 import { Wall } from "./Wall"
-import { Model as Window } from "./Window"
+import { Window } from "./Window"
 export const Room = () => {
 
     return (
         <>
-            {/* Floor */}
             <Floor position={[0, -2 + 0.05, 0]} />
-            <Wall wallSize={[3, 4]} position={[0, 0, -1.6]}  />
-            <Wall wallSize={[3.1, 4]} position={[1.5, 0, -0.05]} rotationY={0.5 * Math.PI} holePosition={[0.8,0]} holeSize={[1,1.5]} />
+            <Wall wallSize={[3, 4]} position={[0, 0, -1.6]} />
+            <Wall wallSize={[3.1, 4]} position={[1.5, 0, -0.05]} rotationY={0.5 * Math.PI} holePosition={[0.5, 0]} holeSize={[1.1, 1.6]} />
             <Wall wallSize={[3.1, 4]} position={[-1.5, 0, -0.05]} rotationY={0.5 * Math.PI} />
-            <Window scale={[0.05 / 0.05000030994415283, 1.5 / 0.5999999940395355, 1 / 0.560000091791153]}  position={[1.55, 0, -0.85]} />
-            <mesh position={[0, -2 +0.55, 0]} castShadow>
-                <boxGeometry args={[1, 1, 1]} />
+            <Window
+                width={1}
+                height={1.5}
+                frameThickness={0.05}
+                position={[1.55, 0, -0.55]}
+                rotation={[0, 0.5 * Math.PI, 0]}
+            />
+            <mesh position={[0, -1 + 0.1, -1]} castShadow>
+                <boxGeometry args={[1, 2, 1]} />
                 <meshStandardMaterial color="red" />
             </mesh>
         </>
