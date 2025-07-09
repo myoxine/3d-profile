4577dba5526d3123cbaaa0068ed5ff62671ba5b7
Hadi Halim <hdi_hlim@yahoo.com>
2025-07-09 16:40:04 +0700

    add desk and chair

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -8,7 +8,7 @@ export const Room = () => {
     return (
         <>
             <Floor position={[0, -2 + 0.05, 0]} />
-            {/* <Wall wallSize={[3, 4]} position={[0, 0, -1.6]} />
+            <Wall wallSize={[3, 4]} position={[0, 0, -1.6]} />
             <Wall wallSize={[3.1, 4]} position={[1.5, 0, -0.05]} rotationY={0.5 * Math.PI} holePosition={[0.5, -0.2]} holeSize={[1.1, 1.6]} />
             <Wall wallSize={[3.1, 4]} position={[-1.5, 0, -0.05]} rotationY={0.5 * Math.PI} />
             <Window
@@ -17,9 +17,9 @@ export const Room = () => {
                 frameThickness={0.05}
                 position={[1.55, -0.2, -0.55]}
                 rotation={[0, 0.5 * Math.PI, 0]}
-            /> */}
+            />
             <Desk scale={[deskScale, deskScale, deskScale]} rotation={[0, -0.5 * Math.PI, 0]} position={[0.75, -2 + 0.1, -1.15]} />
-            <AnimatedSpinningChair  position={[0.25, -2 + 0.7, -0.2]} rotation={[0, -Math.PI, 0]}  />
+            <AnimatedSpinningChair  position={[0.55, -2 + 0.7, -0.2]} rotation={[0, -Math.PI, 0]}  />
         </>
     );
 };
